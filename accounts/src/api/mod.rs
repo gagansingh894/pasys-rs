92e38d6ec06992013bf6d3cba03bc2105e39666f
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-09-14 00:33:05 +0100

    feat: setup api layer for accounts

---
+++ accounts/src/api/mod.rs
@@ -0,0 +1,33 @@
+use crate::service::AccountsService;
+use accounts_proto::accounts_v1::accounts_server::Accounts;
+use accounts_proto::accounts_v1::{
+    CreateAccountRequest, CreateAccountResponse, GetAccountsRequest, GetAccountsResponse,
+};
+use async_trait::async_trait;
+use crate::repo::AccountRepository;
+
+#[async_trait]
+impl <R >Accounts for AccountsService<R>
+where
+    R: AccountRepository,{
+    async fn health_check(
+        &self,
+        request: tonic::Request<()>,
+    ) -> Result<tonic::Response<()>, tonic::Status> {
+        todo!()
+    }
+
+    async fn create_account(
+        &self,
+        request: tonic::Request<CreateAccountRequest>,
+    ) -> Result<tonic::Response<CreateAccountResponse>, tonic::Status> {
+        todo!()
+    }
+
+    async fn get_accounts(
+        &self,
+        request: tonic::Request<GetAccountsRequest>,
+    ) -> Result<tonic::Response<GetAccountsResponse>, tonic::Status> {
+        todo!()
+    }
+}
