bc2a501a86d15dd20a36bd5e6fe20a2af4a0b579
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-09-14 17:17:11 +0100

    feat: setup ledger repo layer

--- ledger/src/repo/mod.rs
+++ ledger/src/repo/mod.rs
@@ -1,1 +1,30 @@
+use async_trait::async_trait;
+use common::database::Database;
 
+#[derive(Debug, Clone)]
+pub struct PgLedgerRepository {
+    db: Database,
+}
+
+impl PgLedgerRepository {
+    pub fn new(db: Database) -> impl LedgerRepository {
+        Self { db }
+    }
+}
+
+#[async_trait]
+pub trait LedgerRepository: LedgerWriter + LedgerReader + 'static + Send + Sync {}
+
+#[async_trait]
+pub trait LedgerWriter: 'static + Send + Sync {}
+
+#[async_trait]
+pub trait LedgerReader: 'static + Send + Sync {}
+
+impl LedgerRepository for PgLedgerRepository {}
+
+#[async_trait]
+impl LedgerReader for PgLedgerRepository {}
+
+#[async_trait]
+impl LedgerWriter for PgLedgerRepository {}
