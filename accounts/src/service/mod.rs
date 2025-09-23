26513708ade48d02acb75f41053ee6375e85c1ea
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-09-24 00:48:43 +0100

    feat: update service layer

--- accounts/src/service/mod.rs
+++ accounts/src/service/mod.rs
@@ -1,6 +1,5 @@
 use crate::domain::account::{Account, Status, Type};
 use crate::repo::AccountRepository;
-use std::sync::Arc;
 
 pub struct AccountsService<R>
 where
@@ -17,19 +16,46 @@ where
         Self { repo }
     }
 
-    pub async fn create_account(account: &Account) -> anyhow::Result<Account> {
-        todo!()
+    pub async fn create_account(
+        &self,
+        name: impl Into<String>,
+        account_type: Type,
+        created_by: impl Into<String>,
+    ) -> anyhow::Result<Account> {
+        let account = Account::new(name.into(), account_type, Status::Active, created_by.into());
+
+        match self.repo.create_account(&account).await {
+            Ok(account) => Ok(account),
+            Err(e) => {
+                anyhow::bail!("Failed to create_account: {:?}", e);
+            }
+        }
     }
 
     pub async fn get_account_by_id(&self, id: &str) -> anyhow::Result<Account> {
-        todo!()
+        match self.repo.get_account_by_id(id).await {
+            Ok(account) => Ok(account),
+            Err(e) => {
+                anyhow::bail!("Failed to get_account_by_id: {:?}", e);
+            }
+        }
     }
 
     pub async fn get_accounts_by_type(&self, account_type: Type) -> anyhow::Result<Vec<Account>> {
-        todo!()
+        match self.repo.get_accounts_by_type(account_type).await {
+            Ok(accounts) => Ok(accounts),
+            Err(e) => {
+                anyhow::bail!("Failed to get_accounts_by_type: {:?}", e);
+            }
+        }
     }
 
     pub async fn get_accounts_by_status(&self, status: Status) -> anyhow::Result<Vec<Account>> {
-        todo!()
+        match self.repo.get_accounts_by_status(status).await {
+            Ok(accounts) => Ok(accounts),
+            Err(e) => {
+                anyhow::bail!("Failed to get_accounts_by_status: {:?}", e);
+            }
+        }
     }
 }
