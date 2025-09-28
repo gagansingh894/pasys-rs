1456ff0893bb82bd01ebb617a43fbf4a6b0e4169
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-09-28 23:20:39 +0100

    fix: grpc client url

--- pasys-api/src/state.rs
+++ pasys-api/src/state.rs
@@ -1,9 +1,6 @@
+use crate::config::Config;
 use accounts_proto::accounts_v1::accounts_client::AccountsClient;
 use tonic::transport::Channel;
-
-pub struct Config {
-    pub accounts_host: String,
-}
 
 pub struct AppState {
     pub accounts_client: AccountsClient<Channel>,
@@ -11,9 +8,17 @@ pub struct AppState {
 
 impl AppState {
     pub async fn new(config: Config) -> Self {
-        let accounts_client = AccountsClient::connect(config.accounts_host.clone())
+        let accounts_client = AccountsClient::connect(get_url(config.accounts_host.clone()))
             .await
             .expect("failed to connect to accounts service");
         Self { accounts_client }
     }
+}
+
+
+pub fn get_url(base_url: String) -> String {
+    if base_url.starts_with("http://") || base_url.starts_with("https://") {
+        return base_url;
+    }
+    format!("http://{}", base_url)
 }
