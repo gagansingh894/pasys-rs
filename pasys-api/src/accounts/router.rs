d3c6c5c2440db5052c3d04e93366305429f318f2
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-09-28 21:24:30 +0100

    feat: add accounts router

---
+++ pasys-api/src/accounts/router.rs
@@ -0,0 +1,11 @@
+use crate::accounts::handlers::{create_account, get_account};
+use crate::state::AppState;
+use axum::Router;
+use axum::routing::{get, post};
+use std::sync::Arc;
+
+pub fn router() -> Router<Arc<AppState>> {
+    Router::new()
+        .route("/account", post(create_account))
+        .route("/account/:id", get(get_account))
+}
